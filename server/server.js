aa94c7a51e908226a317a07f77712a2ced93f83d
KamilaCoder <kamila.spodymek@gmail.com>
2019-05-01 22:07:22 +0200

    Basic server configuration.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const logger = require('morgan');
 const login = require('./routes/login');
 
 
 const app = express();
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+
+// CORS config
+app.use(cors({ credentials: true }));
+
+// Routes
+app.use('/login', login);
+
+
 
 
 //server PORT
@@ -12,9 +24,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
 })
-
-// CORS config
-app.use(cors());
-
-// Routes
-app.use('/login', login);
