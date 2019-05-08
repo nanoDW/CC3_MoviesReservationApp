419150e2a7ea9640790b760bb7fa19b0910373b8
Aaspyr <aaspyr@gmail.com>
2019-05-08 21:44:08 +0200

    movielistfile

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import LoginForm from "./LoginForm";
 import AppHeader from "./AppHeader";
+import MovieList from "./MovieList";
 
 class App extends React.Component {
   state = {};
@@ -11,6 +12,7 @@ class App extends React.Component {
       <>
         <AppHeader loggedIn="true" username="Janusz Kowalski" />
         <LoginForm />
+        <MovieList />
       </>
     );
   }
---
+++ client/src/components/MovieList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+    render(){
+        return <div>Movie List</div>
+    }
+}
+
+export default MovieList;
