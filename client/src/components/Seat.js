e26b6744dc7d57f71b2142597aca8b1745813c4c
Marcin Czajka <czajka.marcin@protonmail.com>
2019-05-12 23:04:13 +0200

    .

--- client/src/components/Seat.js
+++ client/src/components/Seat.js
@@ -7,11 +7,20 @@ class Seat extends React.Component {
         this.state = {seats: this.props.seats}
     }
 
+    selectSeat = (e) => {
+        e.preventDefault()
+        console.log(e.target.innerText)
+    }
+
     render() {
         const result = this.state.seats.map( item => {
             if(item.seat === "false") return <li style={{backgroundColor:"#888"}}></li>
 
-            return <li key={item.row+item.seat}>{item.row}{item.seat}</li>
+            return <li 
+                key={item.row+item.seat} 
+                onClick={this.selectSeat}>
+                {item.row}{item.seat}
+            </li>
         });
 
         return result
