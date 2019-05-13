b86630a9f742b64fda7c043f9c7ab16930175111
Aaspyr <aaspyr@gmail.com>
2019-05-13 19:46:05 +0200

    m

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,37 +1,56 @@
 import React from 'react';
 import axios from 'axios';
-import './MovieList.css';
+import './MovieList.css'
+
+
 class MovieList extends React.Component {
 
     state = {
-        images: []
+        items: [],
+        title: ""
     }
-
+    
     componentDidMount() {
             axios.get(`http://localhost:3000/api/movies/?size=compact&limit=10`)
                 .then(res => {
                     console.log(res);
-                    const photoSmall = res.data[0].imageSmall;
                     this.setState({
-                        images: photoSmall
+                        items: res.data
                     });
-                    console.log(photoSmall);
+                    console.log(res.data);
                 });
             }
-
+            
     render() {
+        var {items} = this.state;
         return (
             <div className="movieList">
                 <div className="movieList_release">
                     <div>Release</div>
                 </div>
+                <div>
+                    {items.map(item => (
+                        <div key={items.genre}>
+                            <img src = {item.imageSmall} alt = "movie poster"/>
+                            {item.title}
+                        </div>
+                    ))};
+                </div>
                 <div className="movieList_today">
                     <div>Today</div>
                 </div>
-                <img src = {this.state.images} alt = "movie poster"/>
             </div>
         );
     }
 }
 
 export default MovieList;
+
+/*<ul>
+                    {items.map(item => (
+                        <li key={items.genre}>
+                            <img src = {item.imageSmall} alt = "movie poster"/>
+                            {item.title}
+                        </li>
+                    ))};
+                </ul>*/
