0f44e2f9b8be1613fc9c41528085cb72dac723f6
Maciej Korolik <maciej.korolik@gmail.com>
2019-05-13 14:46:15 +0200

    MovieDetail component updated, styles updated

---
+++ client/src/components/Schedule.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Placeholder } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+// import moment from 'moment';
+
+import "./Schedule.css";
+
+class Schedule extends React.Component {
+  render() {
+    return (
+      <Placeholder fluid inverted>
+        <Placeholder.Image />
+      </Placeholder>
+    );
+  }
+}
+
+export default Schedule;
