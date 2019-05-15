33056a32b33e56a564ff25736617aee4b9bda312
Aaspyr <aaspyr@gmail.com>
2019-05-15 17:24:18 +0200

    movie

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -27,35 +27,10 @@ class MovieList extends React.Component {
                 <div className="movieList_release">
                     <div>Release</div>
                 </div>
-                <Movie movieinfo = {this.state.movie_info}/>
+                <Movie setMovieId={this.props.setMovieId} movieinfo = {this.state.movie_info}/>
             </div>
         );
     }
 }
 
 export default MovieList;
-
-    /*render() {
-        var {movie_info} = this.state;
-        return (
-            <div className="movieList">
-                <div className="movieList_release">
-                    <div>Release</div>
-                </div>
-                <div className = "movieList_movieImage">
-                    {movie_info.map(item => (
-                        <div key={item._id}>
-                            <button><img className = "movieList_imagesmall" src = {item.imageSmall} alt = "movieList_movie poster"/></button>
-                            <p className = "movieList_title">{item.title}</p>
-                        </div>
-                    ))};
-                    </div>
-                <div className="movieList_today">
-                <div>Today</div>
-                </div>
-            </div>
-                
-
-        );
-    }
-}*/
