1ca89879f7a03dbdcdea30aaa1b38241d60c0a2b
Aaspyr <aaspyr@gmail.com>
2019-05-15 17:03:38 +0200

    movie component +css

---
+++ client/src/components/Movie.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "semantic-ui-css/semantic.min.css";
+import './Movie.css'
+
+const Movie = (props) => {
+    const movies = props.movieinfo.map((info) => {
+    return (
+        <div>
+            <div key={info._id}>
+                <img className = "movie_imagesmall" src={info.imageSmall} alt = "movie poster"/>
+                <div className = "movie_title">{info.title}</div>
+            </div>
+        </div>
+    )
+    });
+
+    return (
+        <div className = "movie_movieInfo"> {movies}</div>  
+    ) 
+};
+
+export default Movie;
--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import "semantic-ui-css/semantic.min.css";
-import './MovieList.css'
+import './MovieList.css';
+import Movie from './Movie.js';
+
 
 
 class MovieList extends React.Component {
 
-    state = {
-        movie_info: [],
-    }
+    state = { movie_info: [] };
     
     componentDidMount() {
-            axios.get(`http://localhost:3000/api/movies`)
+            axios.get(`http://localhost:3000/api/movies/?size=compact&limit=12`)
                 .then(res => {
                     console.log(res);
                     this.setState({
@@ -20,8 +20,22 @@ class MovieList extends React.Component {
                     console.log(res.data);
                 });
             }
-            
+
     render() {
+        return(
+            <div className="movieList">
+                <div className="movieList_release">
+                    <div>Release</div>
+                </div>
+                <Movie movieinfo = {this.state.movie_info}/>
+            </div>
+        );
+    }
+}
+
+export default MovieList;
+
+    /*render() {
         var {movie_info} = this.state;
         return (
             <div className="movieList">
@@ -31,17 +45,17 @@ class MovieList extends React.Component {
                 <div className = "movieList_movieImage">
                     {movie_info.map(item => (
                         <div key={item._id}>
-                            <img className = "movieList_imagesmall" src = {item.imageSmall} alt = "movieList_movie poster"/>
+                            <button><img className = "movieList_imagesmall" src = {item.imageSmall} alt = "movieList_movie poster"/></button>
                             <p className = "movieList_title">{item.title}</p>
                         </div>
                     ))};
-                </div>
+                    </div>
                 <div className="movieList_today">
-                    <div>Today</div>
+                <div>Today</div>
                 </div>
             </div>
+                
+
         );
     }
-}
-
-export default MovieList;
+}*/
